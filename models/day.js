eefbacb782d75000b9da5459f7954ed998ca1838
GitPit <rifflifter@gmail.com>
2021-07-28 18:22:33 -0400

    created day model first draft

---
+++ models/day.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const daySchema = new Schema(
+
+    {
+        day: {
+            type: Date,
+            default: () => new Date()
+        },
+
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Enter 'cardio' or 'resistance'"
+                },
+
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Enter an exercise name"
+                },
+
+                duration: {
+                    type: Number,
+                    required: "Enter an exercise duration in minutes"
+                },
+
+                weight: {
+                    type: Number
+                },
+
+                reps: {
+                    type: Number
+                },
+
+                sets: {
+                    type: Number,
+                }
+            }
+        ]
+    },
+
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
+
+
