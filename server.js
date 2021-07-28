e11696942beca409c660fe289732edff41082854
GitPit <rifflifter@gmail.com>
2021-07-28 19:22:20 -0400

    added Mongo Atlas credentials

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const logger = require("morgan");
+require('dotenv').config();
 
 const Port = process.env.PORT || 3000;
 
@@ -11,9 +12,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/boot-camp-fitness', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
     useFindAndModify: false
 });
 
