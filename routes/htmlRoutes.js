2c8b6f6d51c4eb15f7a5b5428af6e8fc2c166c96
GitPit <rifflifter@gmail.com>
2021-07-28 20:01:51 -0400

    updated htmlRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,9 @@
+const router = require('express').Router();
 const path = require('path');
 
 module.exports = function (app) {
     app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
+        res.sendFile(path.join(__dirname, '..public/index.html'));
     });
 
     app.get('/exercise?', (req, res) => {
@@ -14,3 +15,5 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, '../public/stats.html'));
     });
 };
+
+module.exports = router;
