65134ea2786041164ce1aa431b8e06379aec74c1
GitPit <rifflifter@gmail.com>
2021-07-29 22:38:13 -0400

    seeder working / html routes broken again

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,12 +3,12 @@ const path = require('path');
 
 module.exports = function (app) {
     app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '..public/index.html'));
+        res.sendFile(path.join(__dirname, './index.html'));
     });
 
     app.get('/exercise', (req, res) => {
         console.log('clicked');
-        res.sendFile(path.join(__dirname, '..public/exercise.html'));
+        res.sendFile(path.join(__dirname, '../public/exercise.html'));
     });
 
     app.get('/stats', (req, res) => {
--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
+
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/boot-camp-fitness', {
     useNewUrlParser: true,
